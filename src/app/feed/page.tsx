55821b2aac676d9eb680d88dfd04f1430176a174
Himefe <idanger2015@hotmail.com>
2025-07-16 23:44:14 -0300

    1.0.0

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -6,6 +6,8 @@ export const metadata: Metadata = {
     description: "Esta é a página de feed do projeto dogs!",
 };
 
+export const dynamic = "force-static";
+
 const FeedPage = () => {
     return <FeedWrapper />;
 };
